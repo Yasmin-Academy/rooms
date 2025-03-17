cc4bc0b3fc82cbc55262bbb68f7bbc5b4284a982
Muhammad Isa <service@yasmin.academy>
2025-03-17 16:36:41 -0400

    Update chatrooms.js

--- chatrooms.js
+++ chatrooms.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     if (window.location.href.includes('intermediate-tafsir-en-ar')) {
       const chatIcon = document.createElement('minnit-chat-icon');
-      chatIcon.setAttribute('data-icon-pixel-size', '80');
+      chatIcon.setAttribute('data-icon-pixel-size', '70');
       chatIcon.setAttribute('data-chat-small-medium-or-large', 'small');
       chatIcon.setAttribute('data-circle-or-square', 'circle');
       chatIcon.setAttribute('data-left-or-right', 'right');
       chatIcon.setAttribute('data-chaturl', 'https://organizations.minnit.chat/654987566400689/Main?embed');
-      chatIcon.setAttribute('data-hex-color-code', 'ff0000');
+      chatIcon.setAttribute('data-hex-color-code', '253551');
       chatIcon.style.display = 'block';
 
       document.body.appendChild(chatIcon);
